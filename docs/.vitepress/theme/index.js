50a6e1064307f48baf028d4c8baa960d1ab37ec4
eeymoo <eeymoo@xmail.fun>
2025-08-08 11:04:04 +0800

    feat: 移除不必要的 RepositoryStatus 组件

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,6 +1,5 @@
 import DefaultTheme from 'vitepress/theme'
 import RepoItem from '../components/RepoItem.vue'
-import RepositoryStatus from '../components/RepositoryStatus.vue'
 import RepositoryItem from '../components/RepositoryItem.vue'
 import './custom.css'
 
@@ -8,7 +7,6 @@ export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     app.component('RepoItem', RepoItem)
-    app.component('RepositoryStatus', RepositoryStatus)
     app.component('RepositoryItem', RepositoryItem)
   }
 }
